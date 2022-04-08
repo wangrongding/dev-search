94d81eee12e41812cd5f4a77be818fbc482a6bf2
wangrongding <wangrongding@qq.com>
2022-04-08 20:13:48 +0800

    feat: 兼容多系统,修改了READEME文案

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 一个只有两行代码的命令行工具, 用于在默认浏览器中快速打开 package 在 npm 中的文档
 
+## 安装
+
+```sh
+npm i ding-doc -g
+# or
+yarn global add ding-doc
+```
+
 ## 使用
 
 在命令行中输入 doc + 包名，即可打开该包在 npm 中的文档页面。
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 #! /usr/bin/env node
-const c = require("child_process");
-c.exec(`start https://www.npmjs.com/package/${process.argv[2]}`);
+// const c = require("child_process");
+// c.exec(`start https://www.npmjs.com/package/${process.argv[2]}`);
+
+const open = require("open");
+open("https://www.npmjs.com/package/" + process.argv[2]);
